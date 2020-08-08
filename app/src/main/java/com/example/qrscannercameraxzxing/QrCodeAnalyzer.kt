fec55ca5e69614425e129f207b4d3c7ae4f33a7e
terufumi1122 <terufumi1122@gmail.com>
2020-08-09 08:27:03 +0900

    add QRcodeAnalyzerにYUVフォーマットを指定

--- app/src/main/java/com/example/qrscannercameraxzxing/QrCodeAnalyzer.kt
+++ app/src/main/java/com/example/qrscannercameraxzxing/QrCodeAnalyzer.kt
@@ -1,10 +1,27 @@
 package com.example.qrscannercameraxzxing
 
+import android.graphics.ImageFormat.YUV_420_888
+import android.os.Build
+import android.util.Log
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 
-class QrCodeAnalyzer : ImageAnalysis.Analyzer {
-    override fun analyze(image: ImageProxy) {
+private val TAG = QrCodeAnalyzer::class.java.simpleName
 
+class QrCodeAnalyzer : ImageAnalysis.Analyzer {
+
+    private val yuvFormats = mutableListOf(YUV_420_888)
+
+    init {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            yuvFormats.addAll(listOf(YUV_420_888, YUV_420_888))
+        }
+    }
+
+    override fun analyze(image: ImageProxy) {
+        if (image.format !in yuvFormats) {
+            Log.e(TAG, "Expected YUV, now = ${image.format}")
+            return
+        }
     }
 }
